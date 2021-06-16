e533e1d948ed895682e25edb8963da87925c4395
Milo <milo.christiansen@gmail.com>
2021-06-16 14:47:03 -0400

    Reformat the master index file slightly.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-export { MFootnote, MLeafletMap, MMarkdown, MMobileMenu, MPagination, MSlider, MStickyHeader, MTabs } from "./misc";
 
-export { MAddress, MCheckbox, MCode, MDate, MFile, MNumber, MPhone, MRange, MSelect, MText } from "./form";
+export {
+	MFootnote,
+	MLeafletMap,
+	MMarkdown,
+	MMobileMenu,
+	MPagination,
+	MSlider,
+	MStickyHeader,
+	MTabs
+} from "./misc";
+
+export {
+	MAddress,
+	MCheckbox,
+	MCode,
+	MDate,
+	MFile,
+	MMultiSelect,
+	MNumber,
+	MPhone,
+	MRange,
+	MSelect,
+	MText
+} from "./form";
